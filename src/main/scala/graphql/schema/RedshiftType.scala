8548e4ab312360a8258dfadd360815d9c8bf11fc
Rob Norris <rob_norris@mac.com>
2019-03-20 23:01:34 -0700

    headers

--- src/main/scala/graphql/schema/RedshiftType.scala
+++ src/main/scala/graphql/schema/RedshiftType.scala
@@ -1,3 +1,5 @@
+// Copyright (c) 2019 Association of Universities for Research in Astronomy, Inc. (AURA)
+// For license information see LICENSE or https://opensource.org/licenses/BSD-3-Clause
 
 // Copyright (c) 2019 Association of Universities for Research in Astronomy, Inc. (AURA)
 // For license information see LICENSE or https://opensource.org/licenses/BSD-3-Clause
